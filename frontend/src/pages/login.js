310cac2b44d3de2756ef0a4a45232222fe6ee4b8
Nervado <evandroabreudeoliveira@gmail.com>
2019-08-08 07:01:43 -0300

    30%

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './login.css'
+
+import logo from '../assets/logo.svg'
+import { isContainer } from 'postcss-selector-parser';
+
+export default function login() {
+    return (
+
+
+
+        <img src={logo} alt="Tindev" />)
+}
+
